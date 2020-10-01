32497db0633fd47b5b2ef863fd93bf4f45498cac
ZacharyGopinath <54950453+ZacharyGopinath@users.noreply.github.com>
2020-10-01 00:55:24 -0400

    Delete design2.js

--- designIdeas/design2.js
+++
@@ -1,9 +0,0 @@
-function lightsOff(){
-    document.body.classList.toggle('darkMode');
-    if(document.getElementById('lightSwitch')){
-        document.getElementById('lightSwitch').id = 'onSwitch';
-    }
-    else{
-        document.getElementById('onSwitch').id = 'lightSwitch';
-    }
-}
